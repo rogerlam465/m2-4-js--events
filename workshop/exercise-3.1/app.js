7845a360affd84440a977ccf422acd02e57dca87
rogerlam <rogerlam@wylco.org>
2020-05-03 15:46:53 -0400

    complete to ex 3.2

--- workshop/exercise-3.1/app.js
+++ workshop/exercise-3.1/app.js
@@ -5,27 +5,32 @@
 // coulda done this right in HTML, but whatever.
 // Why do simple when you can do complicated?
 
-for (i = 0; i <= 4; i++) {
-    let rowCreater = document.createElement("div");
-    rowCreater.setAttribute("class", "buttonRow");
-    document.querySelector("#buttonBox").appendChild(rowCreater);
+let buttonHolder = document.createElement("div");
+buttonHolder.setAttribute("id", "buttonHolder");
+document.body.appendChild(buttonHolder);
+
+for (let i = 0; i <= 4; i++) {
+    let buttonRow = document.createElement("div");
+    buttonRow.setAttribute("class", "buttonRow");
+    document.querySelector("#buttonHolder").appendChild(buttonRow);
 }
 
 document.querySelectorAll(".buttonRow").forEach(function (row) {
-    for (j = 0; j <= 3; j++) {
-        let spanner = document.createElement("span");
-        row.appendChild(spanner);
+    for (let j = 0; j <= 3; j++) {
+        let buttonBox = document.createElement("span");
+        buttonBox.setAttribute("class", "buttonBox");
+        row.appendChild(buttonBox);
     }
-})
+});
 
 document.querySelectorAll("span").forEach(function (item) {
-    let buttonCreater = document.createElement("button");
-    buttonCreater.innerText = "Button";
-    item.appendChild(buttonCreater);
-})
+    let buttonMaker = document.createElement("button");
+    buttonMaker.innerText = "Press me";
+    item.appendChild(buttonMaker);
+});
 
-document.querySelectorAll("button").forEach(function (target) {
-    target.addEventListener("click", function () {
-        target.classList.toggle('greenButton');
+document.querySelectorAll("button").forEach(function (item) {
+    item.addEventListener("click", function () {
+        item.classList.add("greenButton");
     })
-})
+});
